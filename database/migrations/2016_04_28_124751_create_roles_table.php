3be156485674d3be8e7adb7aa4cfd9d6f01de27b
Ronauli S <deasnataly@gmail.com>
2016-04-29 14:30:53 +0700

    add migration file

--- database/migrations/2016_04_28_124751_create_roles_table.php
+++ database/migrations/2016_04_28_124751_create_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserRolesTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,9 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->integer('role_id');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +25,6 @@ class CreateUserRolesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('user_roles');
+        Schema::drop('roles');
     }
 }
