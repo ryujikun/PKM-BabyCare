4fc40c7dc27247f24843243ed93c6da77a035f45
Ronauli S <deasnataly@gmail.com>
2016-05-12 00:21:28 +0700

    better with colour dokter peduli view page

--- resources/views/pages/mother/dokterpeduli.blade.php
+++ resources/views/pages/mother/dokterpeduli.blade.php
@@ -18,7 +18,7 @@
     <div class="section white">
         <div class="row container">
             <h2 class="header">Dokter Peduli</h2>
-            <p class="grey-text text-darken-3 lighten-3">
+            <p class=" ">
                 Anda bisa menanyakan pertanyaan disini, dan dokter yang tersedia akan menjawabnya untuk anda.
             </p>
         </div>
@@ -26,13 +26,20 @@
 
             <div class="row">
                 @foreach($items as $item)
-                    <h6> Pada {{$item->created_at}}, anda menanyakan : </h6>
+                    <h5 class="flow-text">Ask</h5>
                     <ul class="collection">
-                        <li class="collection-item">Anda : {{ $item->question }}</li>
-                        @if($item->answer!=NULL)
-                            <li class="collection-item"><strong>Dokter menjawab</strong>: {{ $item->answer }}</li>
-                        @endif
+                        <li class="collection-item red lighten-5">
+                            Anda : {{ $item->question }}
+                        </li>
                     </ul>
+                    @if($item->answer!=NULL)
+                        <h4 class=" flow-text right-align">Answer</h4>
+                        <ul class="collection">
+                            <li class="collection-item green lighten-5">
+                                {{ $item->answer }}
+                            </li>
+                        </ul>
+                    @endif
                     <br>
                 @endforeach
                 <p class="flow-text">Tanyakan pada Dokter</p>
